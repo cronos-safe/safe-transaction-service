5ad1af29faca99dfde5af30b743eb41649013d21
the-amazing-ultraton <guillermo@protofire.io>
2022-07-05 12:13:39 -0300

    fix: #GS-28 revert wrong fix of deterministic deployment proxy address

--- safe_transaction_service/history/management/commands/setup_service.py
+++ safe_transaction_service/history/management/commands/setup_service.py
@@ -390,10 +390,10 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
         ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 2387999),  # v1.3.0
     ],
     EthereumNetwork.REI_TESTNET: [
-        ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 748768),  # v1.3.0
+        ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 748768),  # v1.3.0
     ],
     EthereumNetwork.CRONOS_MAINNET: [
-        ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 3280432),  # v1.3.0
+        ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 3280432),  # v1.3.0
     ],
     EthereumNetwork.CRONOS_TESTNET: [
         ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 3750457),  # v1.3.0
